33259b5794fdab22d6c870578d76fbd18587a11d
adamdelancey <adamdelancey@gmail.com>
2021-01-08 15:36:04 +0000

    finished product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def bag_contents(request):
 
     bag_items = []
@@ -19,32 +18,29 @@ def bag_contents(request):
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
-                'product': product
+                'product': product,
             })
-        else: 
+        else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity
+                total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': item_data,
                     'product': product,
-                    'size': size
+                    'size': size,
                 })
 
-
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(
-            settings.STANDARD_DELIVERY_PERCENTAGE/100)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -52,7 +48,7 @@ def bag_contents(request):
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total
-
+        'grand_total': grand_total,
     }
+
     return context
