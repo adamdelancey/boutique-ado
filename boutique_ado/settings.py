84a04293b2d671cf8ae66a378b316919ec8ec0e2
adamdelancey <adamdelancey@gmail.com>
2021-01-19 12:23:39 +0000

    database fix on heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -116,9 +116,10 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if 'DATABASES_URL' in os.environ:
+
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
@@ -127,7 +128,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
