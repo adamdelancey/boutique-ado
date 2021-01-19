76369d94519fabcae096068075ee986afa0701dd
adamdelancey <adamdelancey@gmail.com>
2021-01-19 11:12:04 +0000

    deployment try again with procfile

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
+SECRET_KEY = 'u$+#-dr9i*ifxdm7u(j-selxq3nq^61@3+17i#uz2f*)v#8)_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
