0d17d2ade9f90074924d3f62e96269113ce1a231
adamdelancey <adamdelancey@gmail.com>
2021-01-19 11:31:00 +0000

    loaddata issues

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u$+#-dr9i*ifxdm7u(j-selxq3nq^61@3+17i#uz2f*)v#8)_6'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
